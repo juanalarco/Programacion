b02bca782e9d6b4f6064ccab63d6103d2f6bcc5e
Juan Alarcon Garijo <jualga01@floridauniversitaria.es>
2016-09-27 13:29:50 +0200

    Ejercicio programacion

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+      //fichero index.php
+//creado por Paco Gomez
+
+        echo "Juan  <br>";
+        echo "Alarco  <br>";
+        print "Garijo  <br>";
+      ?>
+  </head>
+  <body>
+</html>
